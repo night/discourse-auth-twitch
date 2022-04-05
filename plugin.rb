f73eccb2058895e7df52958797ab443c6dd23c00
Bastian Neumann <neumann.bastian@gmail.com>
2022-04-05 18:28:06 +0200

    remove old code

--- plugin.rb
+++ plugin.rb
@@ -25,36 +25,8 @@ class TwitchAuthenticator < ::Auth::ManagedAuthenticator
     SiteSetting.sign_in_with_twitch_enabled?
   end
 
-  def after_authenticate(auth_token)
-    result = Auth::Result.new
-
-    # grab the info we need from omni auth
-    data = auth_token[:info]
-    extra = auth_token[:extra]
-    username = data["nickname"]
-    name = data["name"]
-    email = data["email"]
-    twitch_uid = auth_token["uid"]
-
-    # plugin specific data storage
-    current_info = ::PluginStore.get("twitch", "twitch_uid_#{twitch_uid}")
-
-    result.user =
-      if current_info
-        User.where(id: current_info[:user_id]).first
-      end
-
-    result.username = username
-    result.name = name
-    result.email = email
-    result.extra_data = { twitch_uid: twitch_uid }
-
-    result
-  end
-
-  def after_create_account(user, auth)
-    data = auth[:extra_data]
-    ::PluginStore.set("twitch", "twitch_uid_#{data[:twitch_uid]}", {user_id: user.id })
+  def after_authenticate(auth_token, existing_account: nil)
+    super
   end
 
   def register_middleware(omniauth)
